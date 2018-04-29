b68d9a61e001803ecfcfcca81b3904c9e137d128
otaree <deep.yen.ur.hash@gmail.com>
2018-04-29 12:47:52 +0530

    Add redux extention

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './components/App';
@@ -13,7 +13,9 @@ const rootReducer = combineReducers({
     auth: authReducer,
     blogs: blogReducer
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const jsx = (
     <Provider store={store}>
